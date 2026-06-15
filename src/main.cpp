e43e43a4418a0f65399c316e06f77cd6a1f128b0
JeroEcheverry <214189192+JeroEcheverry@users.noreply.github.com>
2026-06-15 15:50:48 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,9 @@
-
+#include <Arduino.h>
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Firmware iniciando");  
+}
+void loop() {
+  Serial.println("Firmware funcionando");
+  delay(2000);
+}
